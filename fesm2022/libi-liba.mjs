524bb37d85ded4f56d6c7521847c1c1ad81d6bd4
LevFromIKIT <levpalc@gmail.com>
2025-11-11 13:08:03 +0700

    ggg

---
+++ fesm2022/libi-liba.mjs
@@ -0,0 +1,30 @@
+import * as i0 from '@angular/core';
+import { Component } from '@angular/core';
+
+class LibiLiba {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: LibiLiba, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: LibiLiba, isStandalone: true, selector: "ttl-libi-liba", ngImport: i0, template: `
+    <p>
+      libi-liba works!
+    </p>
+  `, isInline: true, styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: LibiLiba, decorators: [{
+            type: Component,
+            args: [{ selector: 'ttl-libi-liba', imports: [], template: `
+    <p>
+      libi-liba works!
+    </p>
+  ` }]
+        }] });
+
+/*
+ * Public API Surface of libi-liba
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { LibiLiba };
+//# sourceMappingURL=libi-liba.mjs.map
---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class LibiLiba {
+    static ɵfac: i0.ɵɵFactoryDeclaration<LibiLiba, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<LibiLiba, "ttl-libi-liba", never, {}, {}, never, never, true, never>;
+}
+
+export { LibiLiba };
